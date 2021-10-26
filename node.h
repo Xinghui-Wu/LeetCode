e5a42c16c9d764dbca4f02147622550dd5309692
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-10-26 20:37:36 +0800

    Define a binary tree.

--- node.h
+++ node.h
@@ -19,4 +19,18 @@ ListNode* create_list(int val_list[], int length);
 // Output the values of the given link list.
 void output_list(ListNode* list);
 
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
 #endif
