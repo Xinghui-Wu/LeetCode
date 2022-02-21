2a538dbb9b78b1620705fbe834cf76fd751e10a0
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-02-21 21:48:07 +0800

    4. Median of Two Sorted Arrays

--- problem_4.cpp
+++ problem_4.cpp
@@ -17,7 +17,6 @@
  */
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -27,18 +26,60 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
     {
-        int length = nums1.size() + nums2.size();
-        vector<int> nums(length);
+        int m = nums1.size();
+        int n = nums2.size();
 
-        merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums.begin());
-        
-        if (length >> 1 << 1 == length)
+        // Make sure that the number of elements in the first vector is not larger than that in the second one.
+        if (m > n)
         {
-            return (double) (nums[(length - 1) / 2] + nums[(length - 1) / 2 + 1]) / 2;
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        // Divide a set into two subsets equally, while the maximum of the first subset is not larger than the minimum of the second subset.
+        // Suppose the first subset has nums1[0] to nums1[i - 1] and nums2[0] to nums2[j - 1] and the second subset has nums1[i] to nums1[m - 1] and nums2[j] to nums2[n - 1].
+        // When m + n is even, i + j = m - i + n - j, and when m + n is odd, i + j = m - i + n - j + 1.
+        // So we can derive i + j = (m + n + 1) / 2, and use i to represent j that j = (m + n + 1) / 2 - i.
+        // We should find i so that nums2[j - 1] <= nums1[i] and nums1[i - 1] <= nums2[j].
+        // It is equivalent to find the largest i that nums1[i - 1] <= nums2[j] with binary search.
+        int i, j;
+        int nums1_i_1, nums1_i, nums2_j_1, nums2_j;
+
+        int left = 0;
+        int right = m;
+
+        int left_max;
+        int right_min;
+
+        while (left <= right)
+        {
+            i = (left + right) / 2;
+            j = (m + n + 1) / 2 - i;
+
+            nums1_i_1 = (i == 0 ? INT32_MIN : nums1[i - 1]);
+            nums1_i = (i == m ? INT32_MAX : nums1[i]);
+            nums2_j_1 = (j == 0 ? INT32_MIN : nums2[j - 1]);
+            nums2_j = (j == n ? INT32_MAX : nums2[j]);
+
+            if (nums1_i_1 <= nums2_j)
+            {
+                left_max = max(nums1_i_1, nums2_j_1);
+                right_min = min(nums1_i, nums2_j);
+
+                left = i + 1;
+            }
+            else
+            {
+                right = i - 1;
+            }
+        }
+        
+        if ((m + n) >> 1 << 1 == (m + n))
+        {
+            return (double) (left_max + right_min) / 2;
         }
         else
         {
-            return (double) nums[length / 2];
+            return (double) left_max;
         }
     }
 };
