d6f8d42bbecca13db465e285a5c930b1240ec6d0
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-02-23 16:39:44 +0800

    15. 3Sum

---
+++ problem_15.cpp
@@ -0,0 +1,95 @@
+/**
+ * 15. 3Sum
+ * 
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ * 
+ * Constraints:
+ *   0 <= nums.length <= 3000
+ *   -10^5 <= nums[i] <= 10^5
+ * 
+ * Difficulty: Medium
+ * Related Topics: Array, Two Pointers, Sorting
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        vector<vector<int>> triplets;
+
+        if (nums.size() < 3)
+        {
+            return triplets;
+        }
+
+        // Sort the vector so that nums[i] <= nums[j] <= nums[k] is guaranteed to avoid duplicate triplets.
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            // Skip this iteration to avoid duplicate triplets.
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k)
+            {
+                // Skip this iteration to avoid duplicate triplets.
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                {
+                    j++;
+                    continue;
+                }
+                
+                if (nums[i] + nums[j] + nums[k] < 0)
+                {
+                    j++;
+                }
+                else if (nums[i] + nums[j] + nums[k] > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    triplets.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                }
+            }
+        }
+
+        return triplets;
+    }
+};
+
+
+int main()
+{
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    Solution solution;
+    vector<vector<int>> triplets = solution.threeSum(nums);
+
+    for (size_t i = 0; i < triplets.size(); i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            cout << triplets[i][j] << "\t";
+        }
+        
+        cout << endl;
+    }
+    
+    return 0;
+}
