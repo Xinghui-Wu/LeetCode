aca2c9617e95b2ef2e9143e8c4cbd2e5320c5f23
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-03-05 17:21:24 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ problem_235.cpp
@@ -0,0 +1,75 @@
+/**
+ * 235. Lowest Common Ancestor of a Binary Search Tree
+ * 
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Constraints:
+ *   The number of nodes in the tree is in the range [2, 10^5].
+ *   -10^9 <= Node.val <= 10^9
+ *   All Node.val are unique.
+ *   p != q
+ *   p and q will exist in the BST.
+ * 
+ * Difficulty: Easy
+ * Related Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+ */
+#include <iostream>
+#include "node.h"
+
+using namespace std;
+
+
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        // If either p or q is found, return it.
+        if (root == nullptr || root == p || root == q)
+        {
+            return root;
+        }
+
+        // Search the left and right subtrees for p and q.
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == nullptr)
+        {
+            return right;
+        }
+        else if (right == nullptr)
+        {
+            return left;
+        }
+        else
+        {
+            return root;
+        }
+    }
+};
+
+
+int main()
+{
+    TreeNode* root = new TreeNode(6);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(0);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(7);
+    root->right->right = new TreeNode(9);
+    root->left->right->left = new TreeNode(3);
+    root->left->right->right = new TreeNode(5);
+
+    TreeNode* p = root->left;
+    TreeNode* q = root->right;
+
+    Solution solution;
+    TreeNode* ancestor = solution.lowestCommonAncestor(root, p, q);
+
+    cout << ancestor->val << endl;
+
+    return 0;
+}
