8e4b6ed28e56f42971d9be5a464ddd68f976c134
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-04-05 20:38:09 +0800

    162. Find Peak Element

--- problem_162.cpp
+++ problem_162.cpp
@@ -25,27 +25,29 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums)
     {
-        int length = nums.size();
+        nums.insert(nums.begin(), INT32_MIN);
+        nums.push_back(INT32_MIN);
 
-        if (length == 1)
-        {
-            return 0;
-        }
-        
-        if (nums[0] > nums[1])
-        {
-            return 0;
-        }
-        if (nums[length - 1] > nums[length - 2])
-        {
-            return length - 1;
-        }
+        int left = 1;
+        int right = nums.size() - 2;
+        int mid;
 
-        for (int i = 1; i < length - 1; i++)
+        while (left <= right)
         {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+            mid = (left + right) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
             {
-                return i;
+                return mid - 1;
+            }
+
+            if (nums[mid] < nums[mid + 1])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
             }
         }
         
